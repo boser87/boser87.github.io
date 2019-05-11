79acb4e23ee3109a721da8b1c1d1fde57a15ece7
Stefano Celentano <stefano.celentano@swedbank.ee>
2019-05-11 09:53:43 +0300

    Try github io publish

---
+++ precache-manifest.e7d13f279636f788a4b98a0b751652cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c8af859f7f557b475e6d46f1e5066f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa1d1f930b69d80cc115",
+    "url": "/static/css/main.703f03e7.chunk.css"
+  },
+  {
+    "revision": "cd4e91a91d037c3390cf",
+    "url": "/static/js/2.9332bc6b.chunk.js"
+  },
+  {
+    "revision": "fa1d1f930b69d80cc115",
+    "url": "/static/js/main.6a6ce701.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
